377393fbabea387e5674c3bb798fe21316c48194
Singee <git@singee.me>
2023-12-25 12:58:45 +0800

    :sparkles: feat(settings): support set key via hash

--- src/app/chat/features/Migration/index.tsx
+++ src/app/chat/features/Migration/index.tsx
@@ -5,9 +5,29 @@ import { createStore, getMany } from 'idb-keyval';
 import dynamic from 'next/dynamic';
 import { PropsWithChildren, memo, useEffect, useState } from 'react';
 
+import { useGlobalStore } from '@/store/global';
+
 import { MIGRATE_KEY, V1DB_NAME, V1DB_TABLE_NAME } from './const';
 
 const Modal = dynamic(() => import('./Modal'), { loading: () => <Spin fullscreen />, ssr: false });
+
+const KeyProvider = memo(() => {
+  const setApiKey = useGlobalStore((s) => s.setOpenAIConfig);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.hash.slice(1));
+    const key = urlParams.get('key');
+    if (key) {
+      console.debug('will setApiKey', key);
+      setApiKey({ OPENAI_API_KEY: key });
+
+      urlParams.delete('key');
+      location.hash = urlParams.toString();
+    }
+  }, [setApiKey]);
+
+  return null;
+});
 
 const Migration = memo<PropsWithChildren>(({ children }) => {
   const [dbState, setDbState] = useState(null);
@@ -36,6 +56,7 @@ const Migration = memo<PropsWithChildren>(({ children }) => {
   return (
     <>
       {open && <Modal open={open} setOpen={setOpen} state={dbState} />}
+      <KeyProvider />
       {children}
     </>
   );
