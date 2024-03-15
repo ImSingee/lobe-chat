c3721bbc6bdb77daeee74efe2aac5a992c4168ea
Bryan <git@singee.me>
2024-03-15 12:22:22 +0800

    :bookmark: chore: do not leak server url

--- src/libs/agent-runtime/utils/desensitizeUrl.ts
+++ src/libs/agent-runtime/utils/desensitizeUrl.ts
@@ -1,34 +1,37 @@
-export const desensitizeUrl = (url: string) => {
-  try {
-    const urlObj = new URL(url);
-    const hostnameParts = urlObj.hostname.split('.');
-    const port = urlObj.port;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const desensitizeUrl = (_url: string) => {
+  return '***';
 
-    // Desensitize domain only if there are at least two parts (example.com)
-    if (hostnameParts.length > 1) {
-      // Desensitize the second level domain (second part from the right)
-      // Special case for short domain names
-      const secondLevelDomainIndex = hostnameParts.length - 2;
-      if (hostnameParts[secondLevelDomainIndex].length < 5) {
-        hostnameParts[secondLevelDomainIndex] = '***';
-      } else {
-        hostnameParts[secondLevelDomainIndex] = hostnameParts[secondLevelDomainIndex].replace(
-          /^(.*?)(\w{2})(\w+)(\w{2})$/,
-          (_, prefix, start, middle, end) => `${prefix}${start}****${end}`,
-        );
-      }
-    }
+  // try {
+  //   const urlObj = new URL(url);
+  //   const hostnameParts = urlObj.hostname.split('.');
+  //   const port = urlObj.port;
 
-    // Join the hostname parts back together
-    const desensitizedHostname = hostnameParts.join('.');
+  //   // Desensitize domain only if there are at least two parts (example.com)
+  //   if (hostnameParts.length > 1) {
+  //     // Desensitize the second level domain (second part from the right)
+  //     // Special case for short domain names
+  //     const secondLevelDomainIndex = hostnameParts.length - 2;
+  //     if (hostnameParts[secondLevelDomainIndex].length < 5) {
+  //       hostnameParts[secondLevelDomainIndex] = '***';
+  //     } else {
+  //       hostnameParts[secondLevelDomainIndex] = hostnameParts[secondLevelDomainIndex].replace(
+  //         /^(.*?)(\w{2})(\w+)(\w{2})$/,
+  //         (_, prefix, start, middle, end) => `${prefix}${start}****${end}`,
+  //       );
+  //     }
+  //   }
 
-    // Desensitize port if present
-    const desensitizedPort = port ? ':****' : '';
+  //   // Join the hostname parts back together
+  //   const desensitizedHostname = hostnameParts.join('.');
 
-    // Reconstruct the URL with the desensitized parts
-    return `${urlObj.protocol}//${desensitizedHostname}${desensitizedPort}${urlObj.pathname}${urlObj.search}`;
-  } catch {
-    // If the URL is invalid, return the original URL
-    return url;
-  }
+  //   // Desensitize port if present
+  //   const desensitizedPort = port ? ':****' : '';
+
+  //   // Reconstruct the URL with the desensitized parts
+  //   return `${urlObj.protocol}//${desensitizedHostname}${desensitizedPort}${urlObj.pathname}${urlObj.search}`;
+  // } catch {
+  //   // If the URL is invalid, return the original URL
+  //   return url;
+  // }
 };
